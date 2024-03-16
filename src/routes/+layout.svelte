05a2631696b13753c24f7a77f3334b897847c6e0
mkpoli <git@mkpo.li>
2024-03-16 15:36:45 +0900

    Add basic analytics

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import { dev } from '$app/environment';
+	import { inject } from '@vercel/analytics';
+	inject({ mode: dev ? 'development' : 'production' });
+</script>
+
+<slot />
