28001830060579c6830c33ec3f70ee6fd39cbce5
mkpoli <git@mkpo.li>
2024-11-27 15:49:31 +0900

    Extract SearchResultCard

---
+++ src/lib/components/SearchResultCard.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import type { Entry } from '$lib/data';
+	import { generateColorHashFromString } from '$lib/genre';
+
+	let { item }: { item: Entry } = $props();
+</script>
+
+<section
+	class="px-6 py-4 shadow-hard border border-black grid grid-cols-[1fr_auto] gap-4 items-start"
+>
+	<h2 class="m-0">{item.Aynu}</h2>
+	<div>
+		<!-- Genre -->
+		<div
+			class={`${generateColorHashFromString(item.sheetName)} text-neutral-800 rounded-md w-max px-2 py-1`}
+		>
+			{item.sheetName}
+		</div>
+	</div>
+	<div class="col-span-2">
+		<p lang="en">{item.English}</p>
+		<p lang="ja">{item.日本語}</p>
+		<p lang="zh">{item.中文}</p>
+	</div>
+</section>
--- src/routes/[query]/+page.svelte
+++ src/routes/[query]/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import Localized from '$lib/components/Localized.svelte';
-	import { generateColorHashFromString } from '$lib/genre';
+	import SearchResultCard from '$lib/components/SearchResultCard.svelte';
 	import type { PageData } from './$types';
 	let { data }: { data: PageData } = $props();
 </script>
@@ -25,23 +25,6 @@
 
 <output class="flex flex-col gap-6">
 	{#each data.found as item}
-		<section
-			class="px-6 py-4 shadow-hard border border-black grid grid-cols-[1fr_auto] gap-4 items-start"
-		>
-			<h2 class="m-0">{item.Aynu}</h2>
-			<div>
-				<!-- Genre -->
-				<div
-					class={`${generateColorHashFromString(item.sheetName)} text-neutral-800 rounded-md w-max px-2 py-1`}
-				>
-					{item.sheetName}
-				</div>
-			</div>
-			<div class="col-span-2">
-				<p lang="en">{item.English}</p>
-				<p lang="ja">{item.日本語}</p>
-				<p lang="zh">{item.中文}</p>
-			</div>
-		</section>
+		<SearchResultCard {item} />
 	{/each}
 </output>
