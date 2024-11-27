fd187335f914a43bb07f3ab7c47c16c9147146ad
mkpoli <git@mkpo.li>
2024-11-28 00:19:36 +0900

    Add Discord link to footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,6 +1,15 @@
+<script lang="ts">
+	import Discord from '~icons/simple-icons/discord';
+</script>
+
 <hr />
 <footer class="p-4 text-center">
-	<p>
+	<p class="flex items-center justify-center gap-2">
 		© 2024 <a href="https://aynu.org/" target="_blank">Aynu.org</a>
+		<a
+			href="https://discord.gg/sJRXq3Gspm"
+			target="_blank"
+			title="Discord ouekarpa / Community / コミュニティ"><Discord class="w-6 h-6" /></a
+		>
 	</p>
 </footer>
