94f3fcbc97a06d1c782832064e24fb9731acd9cc
mkpoli <git@mkpo.li>
2024-11-28 12:10:26 +0900

    Make search result searchable

--- src/lib/components/search/SearchResultCard.svelte
+++ src/lib/components/search/SearchResultCard.svelte
@@ -1,18 +1,20 @@
 <script lang="ts">
 	import type { Entry, Sheet } from '$lib/data';
 	import { formatGenre, generateColorHashFromString } from '$lib/genre';
+	import DividedSearchableTags from '../table/DividedSearchableTags.svelte';
+	import SearchableLink from '../table/SearchableLink.svelte';
 
 	let { item, sheets }: { item: Entry; sheets: Sheet[] } = $props();
 </script>
 
 <section
-	class="px-6 py-4 shadow-hard border border-black grid grid-cols-[1fr_auto] gap-4 items-start"
+	class="grid grid-cols-[1fr_auto] items-start gap-4 border border-black px-6 py-4 shadow-hard"
 >
-	<h2 class="m-0">{item.Aynu}</h2>
-	<div class="flex gap-2 justify-end">
+	<h2 class="m-0"><SearchableLink content={item.Aynu ?? ''} /></h2>
+	<div class="flex justify-end gap-2">
 		<!-- Genre -->
 		<div
-			class={`${generateColorHashFromString(item.sheetName)} text-neutral-800 rounded-md w-max px-2 py-1`}
+			class={`${generateColorHashFromString(item.sheetName)} w-max rounded-md px-2 py-1 text-neutral-800`}
 			title={sheets.find((sheet) => sheet.sheetName === item.sheetName)?.description ??
 				formatGenre(item.sheetName)}
 		>
@@ -20,9 +22,9 @@
 		</div>
 	</div>
 	<div>
-		<p lang="ja">{item.日本語}</p>
-		<p lang="en">{item.English}</p>
-		<p lang="zh">{item.中文}</p>
+		<p lang="ja"><DividedSearchableTags content={item.日本語 ?? ''} language="ja" /></p>
+		<p lang="en"><DividedSearchableTags content={item.English ?? ''} language="en" /></p>
+		<p lang="zh"><DividedSearchableTags content={item.中文 ?? ''} language="zh" /></p>
 	</div>
 	<div>{item['註 / Notes']}</div>
 </section>
