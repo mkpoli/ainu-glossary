d8cc7a5bbadb83dd4c84abb06f7d226dd561810e
mkpoli <git@mkpo.li>
2024-11-28 13:47:48 +0900

    Fix adhoclly uo and uwo and hyphen

--- src/lib/script.svelte.ts
+++ src/lib/script.svelte.ts
@@ -2,9 +2,21 @@ import { browser } from '$app/environment';
 import { convertLatnToKana } from 'ainconv';
 export type Script = 'Kana' | 'Latn';
 
+function latn2kana(latn: string) {
+	const processedLatn = latn
+		.replace(/-/g, ' ')
+		.replace(/\buo/g, 'uwo')
+		.replace(/\bue/g, 'uwe')
+		.replace(/\bio/g, 'iyo')
+		.replace(/\bie/g, 'iye');
+
+	return convertLatnToKana(processedLatn);
+	// return processedLatn;
+}
+
 function createScriptManager() {
 	let script = $state<Script>('Latn');
-	let t = $derived(script === 'Kana' ? convertLatnToKana : (latn: string) => latn);
+	let t = $derived(script === 'Kana' ? latn2kana : (latn: string) => latn);
 
 	return {
 		get script() {
