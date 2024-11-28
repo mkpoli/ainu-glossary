1ea3a3185b389f5f37d895d62cad4549b381a0f4
mkpoli <git@mkpo.li>
2024-11-29 00:53:04 +0900

    Add ScriptSwitch to Header

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,16 +1,23 @@
 <script>
 	import { SITE_TITLE } from '$lib/consts';
+	import ScriptSwitch from '$lib/components/controls/ScriptSwitch.svelte';
+	import T from '$lib/components/ui/T.svelte';
 </script>
 
-<header class="mx-auto flex max-w-prose items-center justify-center gap-2">
-	<picture class="my-4">
-		<source srcset="/android-chrome-192x192.avif" type="image/avif" />
-		<source srcset="/android-chrome-192x192.webp" type="image/webp" />
-		<img src="/android-chrome-192x192.png" alt="Logo" class="h-10 w-10" />
-	</picture>
-	<h1 class="mb-2 mt-2 text-xl md:my-1">
-		<a href="/" class="text-inherit no-underline hover:text-theme-800 hover:underline"
-			>{SITE_TITLE}</a
-		>
-	</h1>
+<header class="mx-auto flex max-w-prose flex-col items-center justify-center gap-2">
+	<div class="flex items-center gap-2">
+		<picture class="my-4">
+			<source srcset="/android-chrome-192x192.avif" type="image/avif" />
+			<source srcset="/android-chrome-192x192.webp" type="image/webp" />
+			<img src="/android-chrome-192x192.png" alt="Logo" class="h-10 w-10" />
+		</picture>
+		<h1 class="mb-2 mt-2 text-xl md:my-1">
+			<a href="/" class="text-inherit no-underline hover:text-theme-800 hover:underline"
+				><T t={SITE_TITLE} /></a
+			>
+		</h1>
+	</div>
+	<div class="ml-auto mr-0 self-end">
+		<ScriptSwitch short />
+	</div>
 </header>
--- src/lib/components/controls/ScriptSwitch.svelte
+++ src/lib/components/controls/ScriptSwitch.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import Switch from '$lib/components/ui/Switch.svelte';
 	import m from '$lib/script.svelte';
+	let { short = false }: { short?: boolean } = $props();
 </script>
 
 <div class="query-form col-span-4 flex items-center justify-center">
@@ -8,6 +9,6 @@
 		checked={m.script === 'Kana'}
 		onchange={(checked) => (m.script = checked ? 'Kana' : 'Latn')}
 	>
-		Romanci{#snippet on()}カタカナ{/snippet}
+		{short ? 'A' : 'Romanci'}{#snippet on()}{short ? 'カ' : 'カタカナ'}{/snippet}
 	</Switch>
 </div>
