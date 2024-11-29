77679eb8e28a4a4ea948cd473e0b6f495832a9b1
mkpoli <git@mkpo.li>
2024-11-29 20:11:44 +0900

    Add highlight to SearchableLink

--- src/lib/components/links/SearchableLink.svelte
+++ src/lib/components/links/SearchableLink.svelte
@@ -1,41 +1,60 @@
 <script lang="ts">
 	import T from '$lib/components/ui/T.svelte';
+	import { segmentWithHighlightIndices } from '$lib/segment';
 	import type { FuseResultMatch } from 'fuse.js';
-
-	let { content, highlight }: { content: string; highlight: FuseResultMatch[] } = $props();
+	import m from '$lib/script.svelte';
+	let {
+		content,
+		highlight
+	}: { content: string; highlight: readonly FuseResultMatch[] | undefined } = $props();
+	const highlightedIndices = $derived(highlight ? highlight.flatMap((h) => h.indices) : []);
 </script>
 
-<span class="relative">
-	{#each highlight as h}
-		{#each h.indices as i}
-			<span
-				class="absolute bottom-1 h-1 bg-yellow-500/30"
-				style={`left: ${i[0]}ch; width: ${i[1] - i[0]}ch`}
-			></span>
-		{/each}
-	{/each}
-
-	<span class="relative z-20">
-		{#each content.split(/([\s,\{\}\?\-\(\)!]+)/u) as word}
-			{#if word.match(/^[VN]\d|YYYY|MM|DD|HH|SS$/)}
-				{word}
-			{:else if word.match(/^[a-zA-Záíúéó='’ ]+$/)}
-				{#if word.includes('=')}
-					{#each word.split(/(=)/) as part}
-						{#if ['a', 'an', '='].includes(part)}
-							<T t={part} />
-						{:else}
-							<a href={`/${part}`} tabindex="-1"><T t={part} /></a>
-						{/if}
-					{/each}
-				{:else}
-					<a href={`/${word}`} tabindex="-1"><T t={word} /></a>
-				{/if}
+<span class="relative z-20">
+	{#each segmentWithHighlightIndices(content, 'ain', highlightedIndices) as { segment, subsegments }}
+		{#if segment.match(/^[VNA]\d|YYYY|MM|DD|HH|SS$/)}
+			{segment}
+		{:else if segment.match(/^[a-zA-Záíúéó='’ ]+$/)}
+			{#if segment.includes('=')}
+				{#each segment.split(/(=)/) as part}
+					{#if ['a', 'an', '='].includes(part)}
+						<T t={part} />
+					{:else}
+						<a href={`/${part}`} tabindex="-1"><T t={part} /></a>
+					{/if}
+				{/each}
 			{:else}
-				{word}
+				<a href={`/${segment}`} tabindex="-1">
+					{#if highlight}
+						{#snippet highlightedText(small: boolean = false)}
+							{#each subsegments as { highlighted, content }}
+								{#if highlighted}
+									<span
+										class="relative after:absolute after:bottom-1 after:left-0 after:right-0 after:-z-10 after:h-1 after:bg-theme-400/40 after:content-['']"
+										class:after:h-[0.2rem]={small}>{content}</span
+									>
+								{:else}
+									{content}
+								{/if}
+							{/each}
+						{/snippet}
+						{#if m.script === 'Kana' && segment.match(/^[a-zA-Záíúéó='’]+$/)}
+							<ruby>
+								<T t={segment} />
+								<rt class="text-sm">{@render highlightedText(true)}</rt>
+							</ruby>
+						{:else}
+							{@render highlightedText()}
+						{/if}
+					{:else}
+						<T t={segment} />
+					{/if}
+				</a>
 			{/if}
-		{/each}
-	</span>
+		{:else}
+			{segment}
+		{/if}
+	{/each}
 </span>
 
 <style>
