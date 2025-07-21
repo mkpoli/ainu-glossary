ffe79b73d5520eb9783a4db049415e1b52689006
mkpoli <git@mkpo.li>
2025-07-22 01:31:21 +0900

    feat: enhance maintenance page with new data fetch buttons and update descriptions

--- src/routes/maintenance/+page.svelte
+++ src/routes/maintenance/+page.svelte
@@ -22,5 +22,23 @@
 			loading = false;
 		}
 	}}
-	disabled={loading}>{loading ? '◉ データ取得中…' : '最新データを取得'}</button
+	disabled={loading}
+	>{loading ? '◉ データ取得中…' : '最新データを取得（Vercel・旧・動かない可能性が大）'}</button
 >
+
+<button
+	onclick={async () => {
+		console.log('Requesting data from Google Sheets...'); // eslint-disable-line no-console
+		loading = true;
+		try {
+			await fetch('https://ainu-glossary-cron-data-update.mkpoli.workers.dev/');
+		} catch (e) {
+			console.error(e); // eslint-disable-line no-console
+		} finally {
+			loading = false;
+		}
+	}}
+	disabled={loading}>{loading ? '◉ データ取得中…' : '最新データを取得（Cloudflare・新）'}</button
+>
+
+<p>週に一回（UTCの日曜日の朝3時、JSTの日曜日の正午12時）自動的に実行されます。</p>
